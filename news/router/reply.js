d07e19c02650145481e6a9fdb82da7d3b54b7f74
DESKTOP-FMVK4Q4\TJ <a@a.a>
2023-10-10 12:30:52 +0900

    노드 뉴스 주성수정

--- news/router/reply.js
+++ news/router/reply.js
@@ -8,16 +8,17 @@ router.use(express.json()) // for parsing application/json
 router.use(express.urlencoded());
 
 //댓글 등록
-router.get('/:nid',(req,res,next) =>{
+router.get('/:nid',(req,res,next) =>{ // 사이트 처음 로드될떄 가져옴
   let nid = req.params.nid;
   const rlist = replyList.filter(reply => reply.nid === nid)
   res.json(rlist)
 })
-router.post('/', (req, res, next) => {
+router.post('/', (req, res, next) => { // post 될시 
   const { nid, replyContent } = req.body;
-  // res.status(201).send('success')
   replyList.push({ nid, replyContent })
-  res.json(replyList)
+  const rlist = replyList.filter(reply => reply.nid === nid)
+  res.json(rlist) // res로 새로운 배열 전송
+  res.status(201).send('success')
   // ejs.renderFile('./template/reply.ejs', { replyList })
   //   .then((data) => res.end(data))
 
