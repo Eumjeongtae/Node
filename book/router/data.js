b00b5c58cd71f14202e8b5292540fbfa6e3b148a
DESKTOP-FMVK4Q4\TJ <a@a.a>
2023-10-12 16:49:05 +0900

    과제 끝

--- book/router/data.js
+++ book/router/data.js
@@ -1,6 +1,6 @@
-const getBooks = () => {
+const getBooks = (category,num) => {
     return new Promise(async (resolve) => {
-      let result = await fetch(`https://www.aladin.co.kr/ttb/api/ItemList.aspx?ttbkey=ttbdmawjdxo121911001&QueryType=ItemNewAll&MaxResults=10&start=1&SearchTarget=Book&output=js&Version=20131101`)
+      let result = await fetch(`https://www.aladin.co.kr/ttb/api/ItemList.aspx?ttbkey=ttbdmawjdxo121911001&QueryType=${category}&MaxResults=${num}&start=1&SearchTarget=Book&output=js&Version=20131101`)
       let data = await result.json()
       resolve(data)
     })
--- book/router/korea.js
+++ book/router/korea.js
@@ -1,13 +1,44 @@
 import ejs from 'ejs'
 import express from 'express'
-import {getBooks} from './data.js'
+import { getBooks } from './data.js'
 const router = express()
-router.get('/',async(req,res,next) =>{
-  ejs.renderFile('./template/list.ejs',{})
-    .then(data=>res.end(data))
-      let bookData = await getBooks()
-      console.log(bookData);
-    
+const category = {
+  ItemNewAll: 'ItemNewAll',
+  ItemNewSpecial: 'ItemNewSpecial',
+  ItemEditorChoice: 'ItemEditorChoice',
+  Bestseller: 'Bestseller',
+  BlogBest: 'BlogBest',
+}
+router.get('/', async (req, res, next) => {
+  let { ItemNewAll, ItemNewSpecial, ItemEditorChoice, Bestseller, BlogBest } = category
+  let newbookData = await getBooks(ItemNewAll, 10)
+  newbookData = newbookData.item;
+
+  let bookDataSpecial = await getBooks(ItemNewSpecial, 10)
+  bookDataSpecial = bookDataSpecial.item;
+
+  let bookBestseller = await getBooks(Bestseller, 10)
+  bookBestseller = bookBestseller.item;
+
+  
+  let bookBlogBest = await getBooks(BlogBest, 10)
+  bookBlogBest = bookBlogBest.item;
+  console.log(bookBlogBest);
+
+
+
+  ejs.renderFile('./template/list.ejs', {newbookData,bookDataSpecial,bookBestseller,bookBlogBest})
+  .then(data=>res.end(data))
+  
 })
+// router.get('/', async (req, res, next) => {
+//   let { ItemNewAll, ItemNewSpecial, ItemEditorChoice, Bestseller, BlogBest } = category
+//   let bookData = await getBooks(ItemNewAll, 10)
+//   res.json(bookData)
+
+
+// })
+
+
 
 export default router
